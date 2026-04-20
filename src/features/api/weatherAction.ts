1a66dc8ab065d1b6fa93975e9c6871446fc8b0dc
RozhR <rozhroman@gmail.com>
2026-04-20 20:47:45 +0300

    Migrate weather feature to RTK Query and refactor Redux slices accordingly.

--- src/features/api/weatherAction.ts
+++ src/features/api/weatherAction.ts
@@ -1,20 +1,15 @@
 import {api_key, base_url} from "../../utils/constants.ts";
-import {createAsyncThunk} from "@reduxjs/toolkit";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import type {WeatherInfoResponse} from "../../utils/types";
 
-export const fetchWeather = createAsyncThunk(
-    'fetch/weather',
-    async (city: string) => {
-        const response = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`)
-        if (!response.ok) {
-            throw new Error('Enter correct city name');
-        }
-        const data = await response.json();
-        return {
-            city: data.name,
-            country: data.sys.country,
-            temp: data.main.temp,
-            pressure: data.main.pressure,
-            sunset: data.sys.sunset * 1000
-        }
-    }
-)
+export const weatherApi = createApi({
+    reducerPath: 'weatherApi',
+    baseQuery: fetchBaseQuery({baseUrl: base_url}),
+    endpoints: builder => ({
+        getWeatherByCity: builder.query<WeatherInfoResponse, string>({
+            query: (city: string) => `?q=${city}&appid=${api_key}&units=metric`,
+        })
+    })
+})
+
+export const {useGetWeatherByCityQuery} = weatherApi
